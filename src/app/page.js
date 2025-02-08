c4921c1db301210725ca0f9ae098e611ffb7135e
Pablo S <salaberry.pablo@gmail.com>
2025-02-08 17:02:39 -0300

    Nuevos componentes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Header from "./Components/Header";
+import Products from "./Components/Products";
+import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Products />
+      <Footer />
     </div>
   );
 }
