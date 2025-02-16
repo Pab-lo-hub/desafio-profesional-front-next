7b2062bed5d2eb4cf99db160ab46ca3061730177
Pablo S <salaberry.pablo@gmail.com>
2025-02-16 14:29:37 -0300

    Modificaciones de estilos

--- src/app/Components/Products.js
+++ src/app/Components/Products.js
@@ -38,7 +38,7 @@ const productsList = [
 
 const Products = () => {
     return (
-        <div className="bg-white">
+        <div className="pt-16">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 className="sr-only">Productos</h2>
 
