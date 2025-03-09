317eb3bbe0d8481f062d7446b985d205536ea527
Pablo S <salaberry.pablo@gmail.com>
2025-03-09 12:20:42 -0300

    Nuevo component Bucador agregados y modificaciones de estilos

---
+++ src/app/Components/Buscador.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Buscador = () => {
+    return (
+        <div className="pt-20">
+            <div className="mx-auto max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl">
+                <h2>Buscador</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Buscador;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import Header from "./Components/Header";
 import Products from "./Components/Products";
 import Footer from "./Components/Footer";
+import Buscador from "./Components/Buscador";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Buscador />
       <Products />
       <Footer />
     </div>
